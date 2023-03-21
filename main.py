8ab668ba5447a6c8ab39bfee884883b191780212
fathimasilviyak <silviyakvm@gmail.com>
2023-03-21 14:41:03 +0530

    turle draw a rectangle

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ flippy.color("chartreuse")
 # Move the tutle forward by 100 place
 flippy.forward(100)
 
+# Draw a rectangle
+for i in range(4):
+    flippy.forward(50)
+    flippy.right(90)
+
 # Move turtle to the origin
 flippy.home()
 
-# Create an object for the Screen class
+# Create an object for the Screen class where the tutle will be shown
 my_screen = Screen()
 
-# Access the attribute of the object to set the screen height
+# Access the attribute of the screen object to show the screen and print screen height
 print(my_screen.canvheight)
 
-# Access the method of object to exit the window when the screen detects a click
+# Access the method of screen object to exit the window when the screen detects a click
 my_screen.exitonclick()
+
